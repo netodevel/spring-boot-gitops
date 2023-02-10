3d502df34e61d7e79ab91b1d4f2be4ecb438acbc
accnetodevel <jose.da.silva.neto@accenture.com>
2023-02-09 22:47:47 -0300

    feat: Adds ChatGPT CodeReview

--- src/main/kotlin/com/netodevel/gitops/GitopsApplication.kt
+++ src/main/kotlin/com/netodevel/gitops/GitopsApplication.kt
@@ -2,7 +2,6 @@ package com.netodevel.gitops
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
